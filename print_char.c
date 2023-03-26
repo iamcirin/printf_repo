c7b23315fc80829d4b377dfca575c845eb3cda59
Zaineb-Hammouda <122837330+Zaineb-Hammouda@users.noreply.github.com>
2023-03-27 00:13:10 +0200

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
-
-int print_char(va_list arg_list){
-
-    _write_char(va_arg(arg_list, int));
-
-    return (1);
-    
-
+/**
+ * print_char - prints a character to stdout
+ * @arg_list: list of arguments sent to printf
+ * Return: 1
+ */
+int print_char(va_list arg_list)
+{
+	_write_char(va_arg(arg_list, int));
+	return (1);
 }
 
+
+/**
+ * print_str - prints a string to stdout
+ * @arg_list: list of arguments sent to printf
+ * Return: number of printed characters
+ */
 
 int print_str(va_list arg_list){
 
@@ -34,6 +41,11 @@ int print_str(va_list arg_list){
 
 }
 
+/**
+ * print_percent - prints % to stdout
+ * @arg_list: list of arguments sent to printf
+ * Return: 1
+ */
 int print_percent(__attribute__((unused))va_list arg_list)
 {
     _write_char('%');
@@ -41,6 +53,12 @@ int print_percent(__attribute__((unused))va_list arg_list)
     return (1);
 
 }
+
+/**
+ * print_num - prints a number to stdout
+ * @arg_list: list of arguments sent to printf
+ * Return: length of number
+ */
 int print_num(int n)
 {
 	int len = 0;
@@ -57,6 +75,13 @@ int print_num(int n)
 	len++;
 	return (len);
 }
+
+/**
+ * print_int - calls print num function
+ * @arg_list: list of arguments sent to printf
+ * Return: length of a number
+ */
+
 int print_int(va_list arg_list){
     
     int n;
@@ -66,8 +91,6 @@ int print_int(va_list arg_list){
 	len = print_num(n);
 
     return (len);
-
-
 }
 
 
